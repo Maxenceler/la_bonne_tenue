388297745b3c9831ac013081165764254bcb4097
Maxence Lerouge <maxencelerouge1@gmail.com>
2021-08-17 14:31:35 +0200

    item index page almost finished (unless footer, banner)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  has_many :bookings
+  has_many :users
+  validates :size, :type, :color, :price, :title, :occasion, :brand, :description, presence: true
+  validates :title, uniqueness: true
 end
