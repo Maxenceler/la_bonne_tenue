6424c9a892149f23d69e7824bf6de56f5f9fbc0b
Maxence Lerouge <maxencelerouge1@gmail.com>
2021-08-17 18:06:50 +0200

    end of the day

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,12 @@ class ItemsController < ApplicationController
 skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @items = Item.where(params[:query] == params)
+    @items = policy_scope(Item)
+    @items = Item.all
+  end
+
+  def filtered_index
+     @items = Item.where(params[:query] == params)
   end
 
   def show
@@ -34,6 +39,6 @@ skip_before_action :authenticate_user!, only: [:index, :show]
   private
 
   def items_params
-    params.require(:item).permit(:title, :size, :color, :price, :occasion, :brand, :description, :photos) ## :type
+    params.require(:item).permit(:title, :size, :color, :price, :occasion, :brand, :description, :photos, :type)
   end
 end
