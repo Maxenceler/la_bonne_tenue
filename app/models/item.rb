36815b1e2940f4b377b590294e5a126b002423ad
violette ruccio <violetteruccio@gmail.com>
2021-08-20 14:30:51 +0200

    show done

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,6 @@ class Item < ApplicationRecord
   validates :title, uniqueness: true
   has_many_attached :photos
   has_one_attached :main_photo
+
+  ITEM_TYPES = {"robe" => "Robes", "mv" => "Manteaux & Vestes", "tb" => "Tops & Blouses", "pj" => "Pantalons & Jeans", "js" => "Jupes & Shorts", "sa" => "Sacs & Accessoires"}
 end
